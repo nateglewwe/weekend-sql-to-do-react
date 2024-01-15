d1a184a8cc22352baadacfa5efdae28fe5779559
Nathaniel Glewwe <nateglewwe@gmail.com>
2024-01-14 19:51:08 -0600

    Added AddTaskForm component and imported into App.jsx

---
+++ src/components/AddTaskForm/AddTaskForm.jsx
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import {useState} from 'react';
+import './AddTaskForm.css';
+
+function AddTaskForm (props) {
+
+    const [taskValue, setTaskValue] = useState('')
+
+    function submitTaskForm(event) {
+        event.preventdefault();
+
+        //pakage data
+        let newTask = {task: taskValue};
+
+        axios.post('/api/todo', newTask)
+        .then((response) => {
+            setTaskValue('');
+            props.refreshTaskListCallback();
+        })
+        .catch((err) => {
+            console.error('ERROR in client POST:', err);
+        });
+    };
+
+    return (
+        <>
+        <h1>Add A Task!</h1>
+        <form onSubmit={submitTaskForm}>
+            <label for="task">Task:</label><br />
+            <input id="taskInput" placeholder="Fight the gods" onChange={(event) => setTaskValue(event.target.value)} value={taskValue}></input>
+            <button type="submit">Add</button>
+        </form>
+        </>
+    )
+}
+
+export default AddTaskForm;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,31 @@
 import {useState} from 'react';
+import AddTaskForm from '../AddTaskForm/AddTaskForm.jsx'
 
 function App () {
+  let [taskList, setTaskList] = useState([])
+  let refreshTaskList = () => {
+    axios.get('/api/todo')
+    .then((response) => {
+      console.table(response.data);
+      setTaskList(response.data);
+    })
+    .catch((err) => {
+      console.error('ERROR in client GET:', err);
+    });
+  };
+
+  //Initial load of component
+  useEffect(() => {
+    // body of effect
+    console.log('Testing initial load of component');
+    // api call
+    refreshTaskList();
+    }, []);
   
   return (
     <div>
       <h1>TO DO APP</h1>
+      <AddTaskForm refreshTaskListCallback={refreshTaskList}/>
     </div>
   );
 
